1e744cf42208cb72e049e9b584c3a051d3a96e4f
joaoehlers <joaofrnd.ehlers@gmail.com>
2026-07-01 20:46:45 -0300

    feat: service base criado

--- src/main/java/com/Home_pocket/service/ListaMercadoService.java
+++ src/main/java/com/Home_pocket/service/ListaMercadoService.java
@@ -7,6 +7,11 @@ import com.Home_pocket.repository.ListaMercadoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
+
 @RequiredArgsConstructor
 @Service
 public class ListaMercadoService {
@@ -22,5 +27,31 @@ public class ListaMercadoService {
                 .nomeItem(itemSalvo.getNomeItem()).precoMedio(itemSalvo.getPrecoMedio()).StFinalizado(itemSalvo.getStFinalizado()).build();
     };
 
+    public ListaMercadoResponseDTO editarProduto(ListaMercadoDTO itemDTO, Long id){
+        ListaMercado itemPraAtt = listaRepository.findById(id).orElseThrow(() -> new RuntimeException("Item da lista nao encontrado"));
 
+        itemPraAtt.setNomeItem(itemDTO.nomeItem());
+        itemPraAtt.setCategoria(itemDTO.categoria());
+        itemPraAtt.setQuantidade(itemDTO.quantidade());
+        itemPraAtt.setPrecoMedio(itemDTO.precoMedio());
+        itemPraAtt.setStFinalizado(itemDTO.StFinalizado());
+
+        listaRepository.save(itemPraAtt);
+
+        return ListaMercadoResponseDTO.builder().id(itemPraAtt.getId()).categoria(itemPraAtt.getCategoria())
+                .nomeItem(itemPraAtt.getNomeItem()).precoMedio(itemPraAtt.getPrecoMedio()).StFinalizado(itemPraAtt.getStFinalizado()).build();
+
+    }
+
+    public List<ListaMercadoResponseDTO> listarLista(){
+        return listaRepository.findAll().stream().map(entidade -> {
+            return ListaMercadoResponseDTO.builder().id(entidade.getId()).categoria(entidade.getCategoria())
+                    .nomeItem(entidade.getNomeItem()).precoMedio(entidade.getPrecoMedio()).StFinalizado(entidade.getStFinalizado()).build();
+        }).toList();
+    }
+
+    public void deletarItemLista(Long id){
+        ListaMercado itemParaDeletar = listaRepository.findById(id).orElseThrow(() -> new RuntimeException("Item nao encontrado"));
+        listaRepository.delete(itemParaDeletar);
+    }
 }
